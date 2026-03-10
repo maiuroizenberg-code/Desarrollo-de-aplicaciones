108c26d778d846cba2ac46f29faa6c49efeba726
maiuroizenberg-code <maiuroizenberg@gmail.com>
2026-03-10 15:41:30 -0300

    Actividad de variables

---
+++ Act n°1 Maia Roiz/src/act/n/pkg1/maia/roiz/ActN1MaiaRoiz.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package act.n.pkg1.maia.roiz;
+
+/**
+ *
+ * @author ET20
+ */
+public class ActN1MaiaRoiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int p=10;
+        System.out.println(p);
+        int q=20;
+        System.out.println(q);
+        float x=2.5f;
+        System.out.println(x);
+        float y=4.3f;
+        System.out.println(y);
+        float z=7.8f;
+        System.out.println(z);
+        char a='M';
+        System.out.println(a);
+        char b = 'A';
+        System.out.println(b);
+        char c = 'I';
+        System.out.println(c);
+        double raiz1 = 5.67;
+        System.out.println(raiz1);
+        double raiz2 = 8.92;
+        System.out.println(raiz2);
+        long contador = 100000;
+        System.out.println(contador);
+        short indicador = 15; 
+        System.out.println(indicador);
+        int indice = 3;
+        System.out.println(indice);
+        double precio = 250.50;
+        System.out.println(precio);
+        double precioFinal = 300.75;
+        System.out.println(precioFinal);
+        char car1 = 'X';
+        System.out.println(car1);
+        char car2 = 'Y';
+        System.out.println(car2);
+        byte valor = 100;
+        System.out.println(valor);
+        boolean primero = true;
+        System.out.println(primero);
+        boolean último = false;
+        System.out.println(último);
+        String nombre = "Maia";
+        System.out.println(nombre);
+        int edad=19;
+        System.out.println(edad);
+        int codigoPostal=1440;
+        System.out.println(codigoPostal);
+        float altura= 1.60f;
+        System.out.println(altura)
+        char genero = “H”
+        System.out.println(genero);
+
+
+
+
+
+
+
+
+
+        // TODO code application logic here
+        
+    }
+    
+}
