4d97172a0b9fef48b44c89b5f7cfe8b0e65bb92a
maiuroizenberg-code <maiuroizenberg@gmail.com>
2026-03-17 17:13:06 -0300

    Actividad ELSE

--- Act n°2 Maia Roiz/src/act/n/pkg2/maia/roiz/ActN2MaiaRoiz.java
+++ Act n°2 Maia Roiz/src/act/n/pkg2/maia/roiz/ActN2MaiaRoiz.java
@@ -1,20 +1,185 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package act.n.pkg2.maia.roiz;
+
+import java.util.Scanner;
 
 /**
  *
- * @author ET20
+ * @author Alumno
  */
-public class ActN2MaiaRoiz {
+public class ActN2MaiaRoiz{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      
+       /**Scanner teclado = new Scanner(System.in);
+
+       
+        System.out.print("Introduce el primer número: ");
+          int num1 = teclado.nextInt();
+
+        System.out.print("Introduce el segundo número: ");
+            int num2 = teclado.nextInt();
+
+        
+       // if (num1 == num2) {
+            System.out.println("Los números " + num1 + " y " + num2 + " son IGUALES.");
+        } else {
+            System.out.println("Los números " + num1 + " y " + num2 + " son DIFERENTES.");
+        }
+
+        
+        //teclado.close();
+        */
+       /** 
+       Scanner teclado = new Scanner(System.in);
+         
+         System.out.print("Introduce el primer número: ");
+          int num1 = teclado.nextInt();
+          
+           System.out.print("Introduce el segundo número: ");
+        int num2 = teclado.nextInt();
+         if (num1 > num2) {
+             System.out.println("El número  " + num1 + " es mayor que " + num2 );
+        } else {
+              System.out.println( "El número " + num2 + " es mayor que  " + num1 );
+        }
+        teclado.close();
+        *  */
+       
+       
+         
+        /**Scanner teclado = new Scanner(System.in);
+
+
+        System.out.print("Introduce el primer número: ");
+            int num1 = teclado.nextInt();
+
+
+        System.out.print("Introduce el segundo número: ");
+           int num2 = teclado.nextInt();
+
+
+        int ultima1 = num1 % 10;
+
+        int ultima2 = num2 % 10;
+
+
+        if (ultima1 == ultima2) {
+
+            System.out.println("Los dos números terminan con la misma cifra.");
+
+        } else {
+
+            System.out.println("Los números terminan con cifras diferentes.");
+
+        }
+
+
+        teclado.close();*/
+        
+        /**Scanner teclado = new Scanner(System.in);
+
+
+        System.out.print("Introduce un número entero: ");
+
+        int numero = teclado.nextInt();
+
+
+        if (numero % 3 == 0 && numero % 5 == 0) {
+
+            System.out.println("El número SI es múltiplo de 3 y de 5.");
+
+        } else {
+
+            System.out.println("El número NO es múltiplo de 3 y de 5.");
+
+        }
+
+
+        teclado.close();*/
+
+   
+       //  Scanner teclado = new Scanner(System.in);
+
+
+        //System.out.print("Introduce el primer número: ");
+
+       // int num1 = teclado.nextInt();
+
+
+     //   System.out.print("Introduce el segundo número: ");
+
+       // int num2 = teclado.nextInt();
+
+
+       // System.out.print("Introduce el tercer número: ");
+
+       // int num3 = teclado.nextInt();
+
+
+      //  int mayor = num1;
+
+
+       // if (num2 > mayor) {
+
+       //     mayor = num2;
+
+      //  }
+
+
+     //   if (num3 > mayor) {
+
+         //   mayor = num3;
+
+      //  }
+
+
+     //   System.out.println("El número mayor es: " + mayor);
+
+
+       // teclado.close();
+       
+       Scanner teclado = new Scanner(System.in);
+
+
+        System.out.print("Introduce una nota entre 0 y 10: ");
+
+        double nota = teclado.nextDouble();
+
+
+        if (nota < 0 || nota > 10) {
+
+            System.out.println("La nota introducida no es válida.");
+
+        } else if (nota < 5) {
+
+            System.out.println("Insuficiente");
+
+        } else if (nota < 6) {
+
+            System.out.println("Suficiente");
+
+        } else if (nota < 7) {
+
+            System.out.println("Bien");
+
+        } else if (nota < 9) {
+
+            System.out.println("Notable");
+
+        } else {
+
+            System.out.println("Sobresaliente");
+
+        }
+
+
+        teclado.close();
+
+   
+
     }
-    
+
 }
