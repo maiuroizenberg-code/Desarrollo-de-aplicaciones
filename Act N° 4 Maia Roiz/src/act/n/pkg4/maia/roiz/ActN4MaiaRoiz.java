64829a11e3633202eaf5aa4bc7b1dd1b8d551957
maiuroizenberg-code <maiuroizenberg@gmail.com>
2026-04-09 15:33:54 -0300

    Actividad While/For

---
+++ Act N° 4 Maia Roiz/src/act/n/pkg4/maia/roiz/ActN4MaiaRoiz.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package act.n.pkg4.maia.roiz;
+
+/**
+ *
+ * @author ET20
+ */
+public class ActN4MaiaRoiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
