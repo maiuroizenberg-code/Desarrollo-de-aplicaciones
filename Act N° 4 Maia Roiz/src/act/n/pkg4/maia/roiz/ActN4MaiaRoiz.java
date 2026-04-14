8794533ea5115ca3da4048ab619d8021cc109c28
maiuroizenberg-code <maiuroizenberg@gmail.com>
2026-04-14 17:30:27 -0300

    Actividad nro 4

--- Act N° 4 Maia Roiz/src/act/n/pkg4/maia/roiz/ActN4MaiaRoiz.java
+++ Act N° 4 Maia Roiz/src/act/n/pkg4/maia/roiz/ActN4MaiaRoiz.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package act.n.pkg4.maia.roiz;
-
+import java.util.Scanner;
 /**
  *
  * @author ET20
@@ -14,7 +14,123 @@ public class ActN4MaiaRoiz {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        /**Ejercicio 1 For
+        int n = 1; 
+           for (int n = 1; n <= 100; n++) {
+            System.out.println(n);
+        }*/ 
+        
+        /**Ejercicio 1 While
+         while(n<=100){
+        System.out.println(n);
+            n++;   
+        }*/
+        /**Ejercicio 1 doWhile
+         do{ 
+          System.out.println(n);
+            n++;
+         } while (n <=100);*/   
+        
+        /**Ejercicio 2 for 
+        System.out.print("Ingrese un numero :");
+        int n = sc.nextInt();
+        
+        for (int t = 1; t <= n; t++) {
+            System.out.println(t);
+        }
+        */
+         /** Ejercicio 2 While
+        System.out.print("Ingrese un numero: ");
+        int n = sc.nextInt();
+
+        int t = 1;
+        while (t <= n) {
+            System.out.println(t);
+            t++;
+        }
+        */
+         
+        /** Ejercicio 2 do While 
+        System.out.print("Ingrese un numero: ");
+        int n = sc.nextInt();
+
+        int t = 1;
+        do {
+            System.out.println(t);
+            t++;
+        } while (t <= n);
+        
+        */
+        
+        /** Ejercicio 3 for
+        System.out.print("Ingrese un numero: ");
+        int n = sc.nextInt();
+
+        for (int t = n; t >= 1; t--) {
+            System.out.println(t);
+        }
+        */
+  
+        
+        
+        /**Ejercicio 3 While 
+        System.out.print("Ingrese un número: ");
+        int n = sc.nextInt();
+        int t= 1;
+        while(t <= n){
+            System.out.println(t);
+            t++;
+        }*/
+        
+        /**Ejercicio 3 doWhile 
+        System.out.println("Ingrese un numero:");
+        int N = sc.nextInt();
+        int t = 1;
+        do{
+            System.out.println(N);
+            N--;
+        } while (t<= N);    
+        */
+        
+        int num1, num2;
+
+        
+        do {
+            System.out.print("Ingrese el primer número: ");
+            num1 = sc.nextInt();
+
+            System.out.print("Ingrese el segundo número: ");
+            num2 = sc.nextInt();
+
+            if (num1 == num2) {
+                System.out.println("Los números son iguales. Intente nuevamente.");
+            }
+        } while (num1 == num2);
+        int min = Math.min(num1,num2);
+        int max = Math.max(num1, num2);
+        for (int a = min; a <= max; a++ ) {
+            System.out.println(a
+            );
+        }
+
+       
+            
+        
+        
+
+    
+    
+    
+    
     }
     
-}
+    
+       
+    
+    
+    
+    }
+    
+    
+
--- JavaApplication39KMC/src/javaapplication39kmc/JavaApplication39KMC.java
+++ JavaApplication39KMC/src/javaapplication39kmc/JavaApplication39KMC.java
@@ -1,24 +1,94 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package javaapplication39kmc;
+class Recurso {
+    String nombre;
 
-import java.util.concurrent.atomic.AtomicInteger;
+    Recurso(String nombre) {
+        this.nombre = nombre;
+    }
+}
 
+class HiloDeadlock extends Thread {
+    private Recurso r1;
+    private Recurso r2;
+
+    HiloDeadlock(String nombre, Recurso r1, Recurso r2) {
+        super(nombre);
+        this.r1 = r1;
+        this.r2 = r2;
+    }
+
+    public void run() {
+        synchronized (r1) {
+            System.out.println(getName() + " bloqueó " + r1.nombre);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {}
+
+            System.out.println(getName() + " espera " + r2.nombre);
+
+            synchronized (r2) {
+                System.out.println(getName() + " bloqueó " + r2.nombre);
+            }
+        }
+    }
+}
+class HiloSeguro extends Thread {
+    private Recurso r1;
+    private Recurso r2;
+
+    HiloSeguro(String nombre, Recurso r1, Recurso r2) {
+        super(nombre);
+        this.r1 = r1;
+        this.r2 = r2;
+    }
+
+    public void run() {
+        Recurso primero = r1.nombre.compareTo(r2.nombre) < 0 ? r1 : r2;
+        Recurso segundo = r1.nombre.compareTo(r2.nombre) < 0 ? r2 : r1;
+
+        synchronized (primero) {
+            System.out.println(getName() + " bloqueó " + primero.nombre);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {}
+
+            synchronized (segundo) {
+                System.out.println(getName() + " bloqueó " + segundo.nombre);
+                System.out.println(getName() + " terminó sin deadlock");
+            }
+        }
+    }
+}
 public class JavaApplication39KMC {
+    public static void main(String[] args){
+ Recurso recursoA = new Recurso("RecursoA");
+        Recurso recursoB = new Recurso("RecursoB");
 
-    private static AtomicInteger contador = new AtomicInteger(0);
+        System.out.println("=== PROVOCANDO DEADLOCK ===");
 
-    public static void main(String[] args)throws InterruptedException {
-        Runnable tarea = () -> {
-            for (int i = 0; i < 1000; i++) contador.incrementAndGet();
-        };
-        Thread h1 = new Thread(tarea);
-        Thread h2 = new Thread(tarea);
-        h1.start(); h2.start();
-        h1.join(); h2.join();
+        Thread t1 = new HiloDeadlock("Hilo-1", recursoA, recursoB);
+        Thread t2 = new HiloDeadlock("Hilo-2", recursoB, recursoA);
 
-        System.out.println("Resultado con Atomic: " + contador.get());
+        t1.start();
+        t2.start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {}
+
+        System.out.println("\n⚠ Los hilos quedaron bloqueados (deadlock).\n");
+
+        System.out.println("=== SOLUCIÓN: EVITANDO DEADLOCK ===");
+
+        Recurso recursoC = new Recurso("RecursoA");
+        Recurso recursoD = new Recurso("RecursoB");
+
+        Thread t3 = new HiloSeguro("Hilo-3", recursoC, recursoD);
+        Thread t4 = new HiloSeguro("Hilo-4", recursoD, recursoC);
+
+        t3.start();
+        t4.start();
     }
 }
